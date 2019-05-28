08cedb2aecd6b6d18a48d6f8482c2d42c367c037
Daniel Bird <danieldbird@gmail.com>
2019-05-28 21:39:12 +1200

    updated with all stats, added yearly progress, this and next year, days and dates.

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
+var weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function updateCalendar() {
-  var now = new Date();
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  let daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-  $(".month-name").html(months[new Date().getMonth()]);
+  Date.prototype.isLeapYear = function() {
+    if ((year & 3) != 0) return false;
+    return year % 100 != 0 || year % 400 == 0;
+  };
 
-  $(".this-year").html(new Date().getFullYear());
+  // Get Day of Year
+  Date.prototype.getDOY = function() {
+    var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    var dayOfYear = dayCount[month] + day;
+    if (month > 1 && this.isLeapYear()) dayOfYear++;
+    return dayOfYear;
+  };
 
-  $(".next-year").html(new Date().getFullYear() + 1);
-
-  let daysInCurrentMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-
-  for (let index = 0; index < daysInCurrentMonth; index++) {
-    $(".month-days").append("<span>" + (index + 1) + "</span>");
+  // check if a leap year
+  function daysInYear(data) {
+    return isLeapYear(data) ? 366 : 365;
   }
+
+  function isLeapYear(data) {
+    return data % 400 === 0 || (data % 100 !== 0 && data % 4 === 0);
+  }
+
+  $(".calendar").prepend("<div class='space'>" + year + "</div>");
+  for (let index = 0; index < daysInMonth; index++) {
+    if (index + 1 === day) {
+      $(".calendar").append(
+        "<div class='space active'><span class='weekday'>" +
+          weekdays[new Date(date.getFullYear(), date.getMonth(), index).getDay()] +
+          "</span><span class='day'>" +
+          (index + 1) +
+          "</span></div>"
+      );
+    } else {
+      $(".calendar").append(
+        "<div class='space'><span class='weekday'>" +
+          weekdays[new Date(date.getFullYear(), date.getMonth(), index).getDay()] +
+          "</span><span class='day'>" +
+          (index + 1) +
+          "</span></div>"
+      );
+    }
+  }
+  $(".calendar").append("<div class='space'>" + (year + 1) + "</div>");
+
+  for (let index = 0; index < daysInMonth; index++) {
+    console.log(index + 1 + weekdays[new Date(date.getFullYear(), date.getMonth(), index).getDay()]);
+  }
+
+  $(".percentage")
+    .html(((date.getDOY() / daysInYear(year)) * 100).toFixed(2) + "%")
+    .css("left", ((date.getDOY() / daysInYear(year)) * 100).toFixed(2) + "%");
+
+  console.log("Full Date: " + date);
+  console.log("Year: " + year);
+  console.log("Month: " + (month + 1));
+  console.log("Days in month: " + daysInMonth);
+  console.log("Days in year: " + daysInYear(year));
+  console.log("Current day of year: " + date.getDOY());
+  console.log("Percentage of year: " + ((date.getDOY() / daysInYear(year)) * 100).toFixed(2));
 }
 
 updateCalendar();
